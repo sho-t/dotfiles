1e0504480d435871234a680839db52d96a55211c
sho tagawa <aznecosann@gmail.com>
2018-04-17 23:33:37 +0900

    :fier: :art: install.shに初期処理つめた

--- install.sh
+++ install.sh
@@ -2,13 +2,19 @@
 
 set -u
 
-DOTPATH="${HOME}/dotfiles"
-GITHUB_URL="https://github.com/sho-t/dotfiles.git"
+declare -r DOTPATH="${HOME}/dotfiles"
+declare -r GITHUB_URL="https://github.com/sho-t/dotfiles.git"
 
+# --------------------------------------------------
+# utilities
+# --------------------------------------------------
 has() {
   type "$1" > /dev/null 2>&1
 }
 
+# --------------------------------------------------
+# download
+# --------------------------------------------------
 if has "git"; then
   echo "Downloading dotfiles..."  
   git clone --recursive "$GITHUB_URL" "$DOTPATH"
@@ -17,3 +23,32 @@ else
   exit 1
 fi
 echo "Download success!"
+
+# --------------------------------------------------
+# link file
+# --------------------------------------------------
+cd $DOTPATH
+echo "start link..."
+for dotfile in .??*
+do
+  [ "$dotfile" = ".git" ] && continue
+  [ "$dotfile" = ".gitmodules" ] && continue
+  [ "$dotfile" = ".gitignore" ] && continue
+
+  ln -Fis "$PWD/$dotfile" $HOME
+done
+echo "Linked success!"
+
+# --------------------------------------------------
+# initialize
+# --------------------------------------------------
+if has "brew"; then
+  echo "Already installed Homebrew ✔︎"
+else
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update && brew upgrade
+brew tap Homebrew/bundle
+brew bundle --file=Brewfile
