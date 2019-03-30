f1f8a709700d61bdef1e47e4d02c87c30e8cab87
sho tagawa <aznecosann@gmail.com>
2019-03-30 19:06:37 +0900

    :star: Added neovim init file

---
+++ src/init/neovim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -r DOTPATH="${HOME}/dotfiles"
+
+# --------------------------------------------------
+# | neovim setup function
+# --------------------------------------------------
+
+cd $DOTPATH
+git submodule foreach git pull origin master
+pip3 install neovim
+nvim :UpdateRemotePlugins
