6d3b7cf2640edb3fd779bb3365a989e2d69b69c7
sho tagawa <aznecosann@gmail.com>
2020-03-10 22:59:41 +0900

    anyenvのscript編集 plugin 入れる

--- src/init/anyenv.sh
+++ src/init/anyenv.sh
@@ -4,8 +4,29 @@
 # | anyenv setup function
 # --------------------------------------------------
 
-if [ ! -d ${HOME}/.anyenv ]; then
-  anyenv install --init
-  anyenv install nodenv
-  exec $SHELL -l
-fi
+init() {
+  if [ ! -d ${HOME}/.anyenv ]; then
+    anyenv install --init
+  fi
+}
+
+install_anyenv_plugins() {
+  mkdir -p $(anyenv root)/plugins
+  git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+  git clone https://github.com/znz/anyenv-git.git $(anyenv root)/plugins/anyenv-git
+}
+
+install_env() {
+    anyenv install nodenv
+    anyenv install rbenv
+    anyenv install goenv
+    exec $SHELL -l
+}
+
+main() {
+  init
+  install_anyenv_plugins
+  install_env
+}
+
+main "$@"
