065cb82c5594f595bbcf4dbb9612101f37ee7fb8
Sho Tagawa <21196267+sho-t@users.noreply.github.com>
2022-10-03 01:58:39 +0900

    fixed script for install homebrew

--- src/brew.sh
+++ src/brew.sh
@@ -13,7 +13,7 @@ initialize() {
     p_info "Already installed Homebrew ✔︎"
   else
     p_start "Installing Homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
   if has "brew"; then
